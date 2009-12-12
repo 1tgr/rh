14ce3fc0313389db5a091b314d47874f3745cce9
Tim Robinson <tim.g.robinson@gmail.com>
2009-12-12 15:00:21 +0000

    Revert "Another test case"
    This reverts commit cc7ba331963c11930886f863e22f1d4c63747a0c.

--- Tests/Main.hs
+++ Tests/Main.hs
@@ -5,30 +5,19 @@ import Rh.Source
 import Test.HUnit
 
 main :: IO Counts
-main = runTestTT $ TestList t
-  where t = [ "Transforming an invalid program should result in an error" ~:
-                let input = "??? LOL PONIES"
-                 in transformFileContents id input ~?= Right "Parse failed at (1:1): Parse error: ???",
+main = runTestTT $ TestList [ "Transforming an invalid program should result in an error" ~:
+                               let input = "??? LOL PONIES"
+                                in transformFileContents id input ~?= Right "Parse failed at (1:1): Parse error: ???",
 
-               "Transforming nothing should change nothing" ~:
-                 let input = "data NotBlah = NotBlah\n"
-                  in transformFileContents id input ~?= Left input,
+                              "Transforming nothing should change nothing" ~:
+                                let input = "data NotBlah = NotBlah\n"
+                                 in transformFileContents id input ~?= Left input,
 
-               "Renaming nonexistent type should change nothing" ~:
-                 let input = "data NotBlah = NotBlah\n"
-                  in transformFileContents (renameType "Blah" "Blah2") input ~?= Left input,
-              
-               "Renaming type without changing spacing" ~:
-                 let input = "data Blah1 = Blah1\n"
-                     expectedProgram = "data Blah2 = Blah1\n"
-                  in transformFileContents (renameType "Blah1" "Blah2") input ~?= Left expectedProgram,
-                  
-               "Renaming type, increasing spacing" ~:
-                  let input = "data Blah = Blah\n"
-                      expectedProgram = "data NewBlah = Blah\n"
-                   in transformFileContents (renameType "Blah" "NewBlah") input ~?= Left expectedProgram,
-                   
-               "Renaming type, decreasing spacing" ~:
-                  let input = "data Blah = Blah\n"
-                      expectedProgram = "data B = Blah\n"
-                   in transformFileContents (renameType "Blah" "B") input ~?= Left expectedProgram ]
+                              "Renaming nonexistent type should change nothing" ~:
+                                let input = "data NotBlah = NotBlah\n"
+                                 in transformFileContents (renameType "Blah" "Blah2") input ~?= Left input,
+                              
+                              "Renaming type without changing spacing" ~:
+                                let input = "data Blah1 = Blah1\n"
+                                    expectedProgram = "data Blah2 = Blah1\n"
+                                 in transformFileContents (renameType "Blah1" "Blah2") input~?= Left expectedProgram ]
