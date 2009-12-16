b906fc8dd29534fb80362c2a6a16fcecc7d1e8f7
Tim Robinson <tim.g.robinson@gmail.com>
2009-12-16 00:03:40 +0000

    WIP on extractValue - currently broken

--- Rh/ExtractValue.hs
+++ Rh/ExtractValue.hs
@@ -9,36 +9,41 @@ import Rh.Refactoring
 dropSrcInfo :: Functor f => f a -> f ()
 dropSrcInfo = fmap (\_ -> ())
 
-replaceSrcInfo :: Functor f => a -> f b -> f a
-replaceSrcInfo a = fmap (const a)
-
-findExp :: Pos -> Pos -> Module SrcSpanInfo -> Exp SrcSpanInfo
-findExp expStart expEnd = head . filter (match . ann) . universeBi
+findExp :: Pos -> Pos -> Module SrcSpanInfo -> Maybe (Exp SrcSpanInfo)
+findExp expStart expEnd m | e:_ <- filter (match . ann) (universeBi m) = Just e
+                          | otherwise = Nothing
   where match loc = let span = srcInfoSpan loc
                         start = (srcSpanStartLine span, srcSpanStartColumn span)
                         end = (srcSpanEndLine span, srcSpanEndColumn span)
                      in start == expStart && end == expEnd
 
-replace :: Exp () -> Exp () -> Exp () -> Maybe (Exp ())
+replace :: Data l => Exp l -> Exp l -> Exp l -> Maybe (Exp l)
 replace matchExp withExp inExp = case runState (descendM replaceM inExp) False of
                                    (replacedExp, True) -> Just replacedExp
                                    (_, False) -> Nothing
-  where replaceM e | e == matchExp = do trace ("\nreplacing \"" ++ (show e) ++ "\" with \"" ++ (show withExp) ++ "\"") $ put True
-                                        return withExp
+  where matchExpMinusSrcInfo = dropSrcInfo matchExp
+        replaceM e | dropSrcInfo e == matchExpMinusSrcInfo = do put True
+                                                                return withExp
                    | otherwise = descendM replaceM e
+
+parseExp :: String -> ParseResult (Exp SrcSpanInfo)
+parseExp str = do m <- parseFileContents ("result = " ++ str)
+                  case m of
+                    Module _ _ _ _ [ PatBind _ _ _ (UnGuardedRhs _ exp) _ ] -> return exp
+                    _ -> fail "Failed to parse expression"
 
 extractValue :: Pos -> Pos -> String -> Module SrcSpanInfo -> Refactor (Module SrcSpanInfo)
 extractValue expStart expEnd name m = rewriteBiM declM m
-  where matchExp = dropSrcInfo $ findExp expStart expEnd m
+  where Just matchExp = findExp expStart expEnd m
         nspan = SrcSpan { srcSpanFilename = "", srcSpanStartLine = 0, srcSpanStartColumn = 0, srcSpanEndLine = 0, srcSpanEndColumn = 0 }
         nloc = SrcSpanInfo { srcInfoSpan = nspan, srcInfoPoints = repeat nspan }
-        withExp = Var () (UnQual () (Ident () name))
+        ParseOk withExp = parseExp name
         declM t | PatBind l p mt (UnGuardedRhs l' r) mb <- t 
-                , Just r' <- replace matchExp withExp (dropSrcInfo r)
-                = let mb' = case mb of
-                              --Just (BDecls l'' binds) -> BDecls l'' (??? : binds)
-                              --Just (IPBinds l'' binds) -> IPBinds l'' (??? : binds)
-                              Nothing -> Just $ BDecls nloc [ PatBind nloc (PVar nloc (Ident nloc name)) Nothing (UnGuardedRhs nloc (replaceSrcInfo nloc withExp)) Nothing ]
-                              _ -> undefined
-                   in return $ Just $ PatBind l p mt (UnGuardedRhs l' (replaceSrcInfo nloc r')) mb'
+                , Just r' <- replace matchExp withExp r
+                = let b' = case mb of
+                             Just (BDecls l'' binds) -> BDecls l'' binds
+                             Just (IPBinds l'' binds) -> IPBinds l'' binds
+                             Nothing -> BDecls nloc [ PatBind nloc (PVar nloc (Ident nloc name)) Nothing (UnGuardedRhs nloc withExp) Nothing ]
+                      p' = PatBind l p mt (UnGuardedRhs l' r') (Just b')
+                   in return $ Just $ trace (show $ dropSrcInfo p') p'
                 | otherwise = return Nothing
